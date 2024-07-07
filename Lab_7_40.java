6e9dfa3c7e1fa1742d8a8da6e6eab01565d91da5
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-07-07 20:03:44 +0530

    Update Lab_7_40.java

--- Lab_7_40.java
+++ Lab_7_40.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-import java.util.Stack;
 public class Lab_7_40{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a string : ");
-		String s = sc.next();
-		Stack<Character> st = new Stack<>();
+		String s = sc.nextLine();
+		Stack st = new Stack(s.length());
 		for(int i=0;i<s.length();i++){
 			if(s.charAt(i) == 'a'){
 			st.push(s.charAt(i));
 			}
 			else if(s.charAt(i) == 'b'){
+				if(st.isEmpty()){
+					System.out.println("Invalid String");
+					return;
+				}
 				st.pop();
 			}
 			else{
@@ -23,6 +26,36 @@ public class Lab_7_40{
 		}
 		else{
 			System.out.println("Invalid String");
+		}
+	}
+}
+class Stack{
+	int top=-1;
+	int n;
+	char[] ch;
+	public Stack(int size){
+		n = size;
+		ch = new char[n];
+	}
+	public void push(char c){
+		if(top>=n-1){
+			System.out.println("Stack Overflow");
+		}
+		else{
+			top = top+1;
+			ch[top] = c;
+		}
+	}
+	public boolean isEmpty(){
+		return top==-1;
+	}
+	public char pop(){
+		if(top<0){
+			return ' ';
+		}
+		else{
+			top = top-1;
+			return ch[top+1];
 		}
 	}
 }
