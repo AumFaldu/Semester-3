489519247c8e1cefbc0f095eab08fb1fba55974b
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-07-16 20:50:28 +0530

    Update Lab-17-2.js

--- NodeProject/Lab-17-2.js
+++ NodeProject/Lab-17-2.js
@@ -3,6 +3,6 @@ console.log(path.basename('C:\\Users\\student\\Desktop\\NodeProject\\Lab-17-2.js
 console.log(path.dirname('/foo/bar/baz/asdf/quux'));
 console.log(path.normalize('C:\\Users\\student\\Desktop\\NodeProject\\Lab-17-2.js'));
 console.log(path.join('/foo','bar','abc'));
-console.log(path.resolve('abc','xyz'));
+console.log(path.resolve('/foo/abc','/foo/abc/xyz'));
 console.log(path.relative('/foo/bar','/foo/abc'));
-console.log(path.extname('path'));
+console.log(path.extname('Lab-17-2.js'));
