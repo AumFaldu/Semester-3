59d45e704a18adcbf37fa308d05b44a1e0f34391
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-07-17 09:21:21 +0530

    Delete Lab-17-3.js

--- Lab-17-3.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-fs.exists('/NodeProject/Lab-17-3.js',(exists)=>{
-	console.log(exists ? 'Found':'Not Found');
-})
-fs.stat('index.js',(err,data)=>{
-	console.log(data);
-})
