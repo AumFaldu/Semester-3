0945fcb4e41bdaddf4f2160f7323670ed968aada
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-07-22 20:48:24 +0530

    Update Project.js

--- NodeProject/Project/Project.js
+++ NodeProject/Project/Project.js
@@ -4,8 +4,6 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">');
-
-    // Navbar
     res.write(`
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
