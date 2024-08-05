23f87e4ece7698ee5171ee231035ef5bc26ccbb6
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-08-05 08:53:59 +0530

    Update Lab_19_92.java

--- Lab_19_92.java
+++ Lab_19_92.java
@@ -12,16 +12,16 @@ public class Lab_19_92{
         bubble_sort(arr);
         System.out.println("Sorted Array using Bubble sort : ");
         for(int i=0;i<n;i++){
-            System.out.println("arr[i] : "+arr[i]);
+            System.out.println("arr["+i+"] : "+arr[i]);
         }
     }
     private static void bubble_sort(int[] a){
         for(int i=0;i<a.length-1;i++){
-            for(int j=i+1;j<a.length;j++){
-                if(a[i]>a[j]){
-                    int temp=a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp=a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
                 }
             }
         }
