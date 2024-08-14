bf86a0de5e260ddc2a9689409e3823fba09e7a51
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-08-14 16:19:34 +0530

    Delete Lab-21-3 directory

--- Lab-21-3/model.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const schema = mongoose.Schema({
-    id:Number,
-    Name:String,
-    Class:String,
-    RollNo:Number
-});
-module.exports = mongoose.model('Students',schema);
