da78edc3e6fd359b36e1bf739990aa47f92f1dad
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-08-28 19:14:24 +0530

    Update Lab_21_5.js

--- ExpressProject/Lab-21-5/Lab_21_5.js
+++ ExpressProject/Lab-21-5/Lab_21_5.js
@@ -3,44 +3,44 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 require('dotenv').config();
-const model = require('./model');
+const Product = require('./Product');
 const connectionString  = "mongodb+srv://"+process.env.DB_UserName+":"+process.env.DB_Password+"@cluster0.0w7pb.mongodb.net/Demo";
 mongoose.connect(connectionString).then(()=>{
     console.log('MongoDB Connected');
     //GetAll
     app.get('/',async (req,res)=>{
-        const ans = await model.find();
+        const ans = await Product.find();
         res.send(ans);
     });
     //Add
     app.post('/add',async (req,res)=>{
         const {id,Name,Class,RollNo} = req.body;        
-        const ans = await model.create({...req.body});
+        const ans = await Product.create({...req.body});
         res.send('Added Successfully');
     });
     //GetXByParameter
     app.get('/one/:id',async (req,res)=>{
-        const data = await model.findOne({id:req.params.id});
+        const data = await Product.findOne({id:req.params.id});
         res.send(data);
     })
     //GetXByID
     app.get('/:findId',async (req,res)=>{
-        const data = await model.findById(req.params.findId);
+        const data = await Product.findById(req.params.findId);
         res.send(data);
     })
     //Update
     app.put('/:id',async (req,res)=>{
-        await model.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+        await Product.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
         res.send('Updated Successfully');
     });
     //Delete
     app.delete('/:id',async (req,res)=>{
-        await model.deleteOne({id:req.params.id});
+        await Product.deleteOne({id:req.params.id});
         res.send('Deleted Successfully')
     });
      //Search
     app.get('/search/:s',async (req,res)=>{
-        const ans = await model.find({
+        const ans = await Product.find({
             Name:{
                 $regex : req.params.s
             }
