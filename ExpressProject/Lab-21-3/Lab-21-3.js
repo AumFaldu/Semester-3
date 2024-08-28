8a4af5cdf2a9c35a916ff866e1c144e7f9a59e3c
Aum Faldu <140812087+AumFaldu@users.noreply.github.com>
2024-08-28 19:10:40 +0530

    Update Lab-21-3.js

--- ExpressProject/Lab-21-3/Lab-21-3.js
+++ ExpressProject/Lab-21-3/Lab-21-3.js
@@ -3,44 +3,44 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 require('dotenv').config();
-const model = require('./model');
+const Student = require('./Student');
 const connectionString  = "mongodb+srv://"+process.env.DB_UserName+":"+process.env.DB_Password+"@cluster0.0w7pb.mongodb.net/Demo";
 mongoose.connect(connectionString).then(()=>{
     console.log('MongoDB Connected');
     //GetAll
     app.get('/',async (req,res)=>{
-        const ans = await model.find();
+        const ans = await Student.find();
         res.send(ans);
     });
     //Add
     app.post('/add',async (req,res)=>{
         const {id,Name,Class,RollNo} = req.body;        
-        const ans = await model.create({...req.body});
+        const ans = await Student.create({...req.body});
         res.send('Added Successfully');
     });
     //GetXByParameter
     app.get('/one/:id',async (req,res)=>{
-        const data = await model.findOne({id:req.params.id});
+        const data = await Student.findOne({id:req.params.id});
         res.send(data);
     })
     //GetXByID
     app.get('/:findId',async (req,res)=>{
-        const data = await model.findById(req.params.findId);
+        const data = await Student.findById(req.params.findId);
         res.send(data);
     })
     //Update
     app.patch('/:id',async (req,res)=>{
-        await model.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+        await Student.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
         res.send('Updated Successfully');
     });
     //Delete
     app.delete('/:id',async (req,res)=>{
-        await model.deleteOne({id:req.params.id});
+        await Student.deleteOne({id:req.params.id});
         res.send('Deleted Successfully')
     });
     //Search
     app.get('/search/:s',async (req,res)=>{
-        const ans = await model.find({
+        const ans = await Student.find({
             Name:{
                 $regex : req.params.s
             }
